2431bcb2b85f18bfe4e54407c5e8c6b8c7ba5dab
VieZhong <zyw13579@qq.com>
2017-01-09 20:03:12 +0800

    clear keywords

--- app/components/Search/index.jsx
+++ app/components/Search/index.jsx
@@ -6,14 +6,29 @@ const propTypes = {
     keyWordChanged: PropTypes.func.isRequired
 }
 
-function Search({keyWordChanged: onValueChanged}) {
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onValueChanged = this.props.keyWordChanged;
+        this.clearKeyWord = this.clearKeyWord.bind(this);
+    }
 
-    return (
-        <div className="search">
-            关键字:<input onChange={onValueChanged} name="keywords"/>
-        </div>
-    );    
+    clearKeyWord() {
+        this.onValueChanged({
+            target: {
+                value: ''
+            }
+        });
+        this.refs.keywords.value = '';
+    }
 
+    render() {
+        return (
+            <div className="search">
+                关键字:<input ref="keywords" onChange={this.onValueChanged} name="keywords"/><button onClick={this.clearKeyWord}>清空</button>
+            </div>
+        );         
+    }
 }
 
 Search.propTypes = propTypes;
