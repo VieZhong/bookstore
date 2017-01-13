103bbb8a7c981bad3f5e9a5f92b94e1ea5be3304
VieZhong <zyw13579@qq.com>
2017-01-13 13:24:56 +0800

    connect koa.js back end

--- app/app.jsx
+++ app/app.jsx
@@ -15,25 +15,40 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hasReadBooks: hasReadBooks,
-            willReadBooks: willReadBooks,
+            booksList: [],
             active: 'hasRead'
         };
         this.changeActiveState = this.changeActiveState.bind(this);
+        this.getBooksList = this.getBooksList.bind(this);
+    }
+    getBooksList(state) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = () => {
+            if(xmlHttp.readyState==4) {
+                this.setState({
+                    'booksList': JSON.parse(xmlHttp.responseText)
+                });
+            }
+        };
+        xmlHttp.open("GET", "http://localhost/api/bookstore/books/" + state, true);
+        xmlHttp.send();
     }
     changeActiveState(state) {
         this.setState({
             'active': state
         });
+        this.getBooksList(state);
+    }
+    componentDidMount() {
+        this.getBooksList(this.state.active);
     }
     render() {
-        const booksList = this.state.active == 'hasRead' ? this.state.hasReadBooks : this.state.willReadBooks;
         return (
             <div>
                 <Header></Header>
                 <div>
                     <NavList active={this.state.active} changeState={this.changeActiveState}/>
-                    <BookList books={booksList}></BookList>
+                    <BookList books={this.state.booksList}></BookList>
                 </div>
                 <Footer></Footer>
             </div>
